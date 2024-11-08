159e42269b8e135545ec44b1bee4a006d067dfd2
cherry_77 <1615405@qq.com>
2024-11-08 10:18:05 +0800

    Create 03_getinfo.cpp

---
+++ chapter_02/programs/03_getinfo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int carrots;
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "Here are two more. ";
+    carrots = carrots + 2;
+    cout << "Now you have " << carrots << " carrots." << endl;
+    return 0;
+}
+
+// How many carrots do you have?
+// 12
+// Here are two more. Now you have 14 carrots.
